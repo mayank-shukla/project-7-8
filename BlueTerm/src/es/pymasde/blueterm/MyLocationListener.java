32c555f794f236b8c82f1544b4553b3524473f6b
c.w.hoe01 <c.w.hoe01@gmail.com>
2012-07-08 12:59:09 +0000

    Bluetooth connection doet het, zonder gps loc en berekeningen

---
+++ BlueTerm/src/es/pymasde/blueterm/MyLocationListener.java
@@ -0,0 +1,23 @@
+package es.pymasde.blueterm;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+public class MyLocationListener implements LocationListener {
+	private BlueTerm main;
+
+	public MyLocationListener(BlueTerm main) {
+		this.main = main;
+	}
+
+	public void onLocationChanged(Location loc) {
+		main.setlocChange(loc.getLongitude(),loc.getLatitude());
+	}
+
+	public void onProviderDisabled(String arg0) {}
+
+	public void onProviderEnabled(String arg0) {}
+
+	public void onStatusChanged(String provider, int status, Bundle extras) {}
+}
