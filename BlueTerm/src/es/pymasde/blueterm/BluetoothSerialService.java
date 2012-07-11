242a116512a49e57d709a8e060e1592d4e80fbfd
jimmyvanhest@gmail.com <jimmyvanhest@gmail.com@baf0e1cf-f7fb-78a7-3e9e-777a31e8913f>
2012-07-11 12:54:51 +0000

    CTRL+SHIFT+F

--- BlueTerm/src/es/pymasde/blueterm/BluetoothSerialService.java
+++ BlueTerm/src/es/pymasde/blueterm/BluetoothSerialService.java
@@ -47,10 +47,10 @@ public class BluetoothSerialService {
 	private int mState;
 	private EmulatorView mEmulatorView;
 	// Constants that indicate the current connection state
-	public static final int STATE_NONE = 0;       // we're doing nothing
-	public static final int STATE_LISTEN = 1;     // now listening for incoming connections
+	public static final int STATE_NONE = 0; // we're doing nothing
+	public static final int STATE_LISTEN = 1; // now listening for incoming connections
 	public static final int STATE_CONNECTING = 2; // now initiating an outgoing connection
-	public static final int STATE_CONNECTED = 3;  // now connected to a remote device
+	public static final int STATE_CONNECTED = 3; // now connected to a remote device
 
 	/**
 	 * Constructor. Prepares a new BluetoothChat session.
