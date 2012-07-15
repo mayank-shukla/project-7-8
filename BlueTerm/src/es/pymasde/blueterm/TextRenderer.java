7703d9ae248ad64625cd6406888579a0b46e381d
c.w.hoe01@gmail.com <c.w.hoe01@gmail.com@baf0e1cf-f7fb-78a7-3e9e-777a31e8913f>
2012-07-15 15:33:13 +0000

    Refactored

---
+++ BlueTerm/src/es/pymasde/blueterm/TextRenderer.java
@@ -0,0 +1,15 @@
+package es.pymasde.blueterm;
+
+import android.graphics.Canvas;
+
+/**
+ * Text renderer interface
+ */
+interface TextRenderer
+{
+int getCharacterWidth();
+
+int getCharacterHeight();
+
+void drawTextRun(Canvas canvas, float x, float y, int lineOffset, char[] text, int index, int count, boolean cursor, int foreColor, int backColor);
+}
